a77987d08353a84ffaf269e96928761bae9508ed
AbyssRT <djabyss@yandex.ru>
2022-07-18 19:23:25 +0300

    поправил всё кроме статик флага (не могу убрать статик свойство)

--- src/main/java/Main1000to1.java
+++ src/main/java/Main1000to1.java
@@ -1,9 +1,10 @@
 import java.io.PrintStream;
 
 public class Main1000to1 {
-    static final PrintStream sout = System.out;
+
 
     public static void main(String[] args) {
+        PrintStream sout = System.out;
         for(int i=1000; i>0; i = i-2) {
             sout.println(i);
         }
--- src/main/java/MainCalc.java
+++ src/main/java/MainCalc.java
@@ -1,22 +1,22 @@
 import javax.naming.Context;
 import java.io.PrintStream;
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainCalc {
-    static final PrintStream sout = System.out;
-    static final Scanner scn = new Scanner(System.in);
     static boolean flagOk;
-    static String strtype;
-    static boolean zeroErr;
 
     public static void main(String[] args) {
+        PrintStream sout = System.out;
         float var1;
         float var2;
-        byte oper;
+        String oper;
         String out;
+        Scanner scn = new Scanner(System.in);
 
         while (true) {
-            zeroErr = false;
             flagOk = true;
             do {
                 sout.print("Введите первый операнд: ");
@@ -36,13 +36,8 @@ public class MainCalc {
             } while (flagOk);
 
             double res = calculate(var1, var2, oper);
-            out =  String.format( "Результат: %s %s %s = ",
-                    var1, strtype, var2);
-            if (zeroErr) {
-                out = out + "Ошибка! Деление на 0 даёт неопределённость";
-            } else {
-                out = out + String.valueOf(res);
-            }
+            out =  String.format( "Результат: %s %s %s = %s",
+                    var1, oper, var2, res);
             sout.println(out);
         }
     }
@@ -53,27 +48,27 @@ public class MainCalc {
         try {
             f = Float.parseFloat(instr);
             flagOk = false;
-        } catch (Exception e) {
-            sout.println("Нужно писать только число. Для нецелых чисел разделитель точка. Пример: 3.14");
+        } catch (NumberFormatException e) {
+            System.out.println(e.toString());
+            System.out.println("Нужно писать только число. Для нецелых чисел разделитель точка. Пример: 3.14");
         }
         return f;
     }
 
-    static byte validatorOpertr(String instr) {
+    static String validatorOpertr(String instr) {
         StopCheck(instr);
-        byte o = 0;
-        switch (instr) {
-            case "+": o = 1; break;
-            case "-": o = 2; break;
-            case "*": o = 3; break;
-            case "/": o = 4; break;
-            default: sout.println("Неизвестный оператор. Требуется оператор только +,-,*,/");
+        String[] opers = new String[] { "+", "-", "*", "/" };
+        String out = "";
+        for (String oper: opers) {
+            if (instr.equals(oper)) {
+                out = instr;
+                break;
+            }
         }
-        if (o != 0) {
+        if (!"".equals(out)) {
             flagOk = false;
-            strtype = instr;
         }
-        return o;
+        return out;
     }
 
     static void StopCheck (String instr) {
@@ -82,21 +77,14 @@ public class MainCalc {
         }
     }
 
-    static double calculate(float var1, float var2, byte type) {
-        double d = 0;
+    static double calculate(float var1, float var2, String type) {
+        double out = 0;
         switch(type) {
-            case 1: d = var1 + var2; break;
-            case 2: d = var1 - var2; break;
-            case 3: d = var1 * var2; break;
-            case 4:
-                if (var2 != 0) {
-                    d = var1 / var2;
-                } else {
-                    d = 0;
-                    zeroErr = true;
-                }
-                break;
+            case "+": out = var1 + var2; break;
+            case "-": out = var1 - var2; break;
+            case "*": out = var1 * var2; break;
+            case "/": out = var1 / var2; break;
         }
-        return d;
+        return out;
     }
 }
